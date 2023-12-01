292c8127e89356582495960707c934d59a8dace3
Nilson Aguiar <naguiar@bol.com>
2023-12-01 20:02:53 +0100

    Day one

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,10 @@ plugins {
     kotlin("jvm") version "1.9.20"
 }
 
+dependencies {
+    implementation("org.assertj:assertj-core:3.11.1")
+}
+
 sourceSets {
     main {
         kotlin.srcDir("src")
--- src/Day01.kt
+++ src/Day01.kt
@@ -1,6 +1,13 @@
+import org.assertj.core.api.Assertions.assertThat
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.sumOf {
+            val first = it.firstNotNullOf { char -> char.digitToIntOrNull() }
+            val last = it.reversed().firstNotNullOf { char -> char.digitToIntOrNull() }
+
+            "$first$last".toInt()
+        }
     }
 
     fun part2(input: List<String>): Int {
@@ -9,7 +16,7 @@ fun main() {
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    assertThat(part1(testInput)).isEqualTo(142)
 
     val input = readInput("Day01")
     part1(input).println()
