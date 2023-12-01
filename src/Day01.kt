83e03f4565fae03ab3c4c29114d04f7530bd2eac
Nilson Aguiar <naguiar@bol.com>
2023-12-01 21:22:27 +0100

    Day one part2

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,5 +1,9 @@
 import org.assertj.core.api.Assertions.assertThat
 
+enum class Digit {
+    zero, one, two, three, four, five, six, seven, eight, nine
+}
+
 fun main() {
     fun part1(input: List<String>): Int {
         return input.sumOf {
@@ -10,13 +14,42 @@ fun main() {
         }
     }
 
+    fun Int.foundOr(orValue: Int) =
+        if (this == -1) orValue
+        else this
+
+    fun Digit.findFirstIndex(string: String): Int =
+        minOf(
+            string.indexOf(name, ignoreCase = true).foundOr(Int.MAX_VALUE),
+            string.indexOf("$ordinal").foundOr(Int.MAX_VALUE)
+        )
+
+    fun Digit.findLastIndex(string: String): Int =
+        maxOf(
+            string.indexOf(name, ignoreCase = true).foundOr(Int.MIN_VALUE),
+            string.indexOf("$ordinal").foundOr(Int.MIN_VALUE)
+        )
+
     fun part2(input: List<String>): Int {
-        return input.size
+        return input.sumOf { string ->
+            val first = Digit.entries.map { digit ->
+                Pair(digit, digit.findFirstIndex(string))
+            }.filter { it.second >= 0 }.minBy { it.second }.first
+
+            val last = Digit.entries.map { digit ->
+                Pair(digit, digit.findLastIndex(string))
+            }.filter { it.second >= 0 }.maxBy { it.second }.first
+
+            "${first.ordinal}${last.ordinal}".toInt()
+        }
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    assertThat(part1(testInput)).isEqualTo(142)
+    val part1TestInput = readInput("Day01_test")
+    assertThat(part1(part1TestInput)).isEqualTo(142)
+
+    val part2TestInput = readInput("Day01_test2")
+    assertThat(part2(part2TestInput)).isEqualTo(281)
 
     val input = readInput("Day01")
     part1(input).println()
